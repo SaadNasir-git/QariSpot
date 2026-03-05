192b6ed6b5cc84ee17781c44ab2b0b6b1286dae8
Saad Nasir <mn0871590@gmail.com>
2026-03-05 23:54:09 +0500

    Make Home component async and await fetchQaris

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,21 @@ import getDatabaseConnection from "@/lib/mysql2";
 import HomePage from "./HomePage";
 import { RowDataPacket } from "mysql2";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const fetchQaris = async () => {
   try {
     const conn = await getDatabaseConnection();
     const [data] = await conn.query<RowDataPacket[] & qari[]>('SELECT * FROM qari');
     return data
   } catch (error) {
-    console.log(error)
     return []
   }
 }
 
-const Home = () => {
-  let Qaris = fetchQaris()
+const Home = async () => { // Make this async
+  let Qaris = await fetchQaris() // Add await here
 
   return (
     <HomePage fetchedQaris={Qaris} />
