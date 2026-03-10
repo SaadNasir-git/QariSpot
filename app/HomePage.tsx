f3a274df92a9a5496b1695ad92c40624976ff255
Saad Nasir <mn0871590@gmail.com>
2026-03-10 09:55:20 +0500

    Update HomePage.tsx

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -10,7 +10,7 @@ const HomePage = ({ fetchedQaris }: { fetchedQaris: Promise<qari[]> }) => {
     return (
         <div className='grid grid-cols-2 sm:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-2 transition-all duration-500 ease-in-out px-4 sm:px-0'>
             {Qaris.map((qari) => (
-                <Link href={qari.id.toString()} className="w-full">
+                <Link href={qari.id.toString()} className="w-full flex justify-center">
                     <Card QariName={qari.name} imageUrl={qari.picUrl} />
                 </Link>
             ))}
