2f91c3e75a00a3ae08a11b9ec355a36286d7cd60
Saad Nasir <mn0871590@gmail.com>
2026-03-14 14:14:19 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    serverExternalPackages: ['fluent-ffmpeg', 'ffmpeg-static'],
     async headers() {
         return [
             {
