40fe88957c93be290eb1726d89ce3c0ed55ef20e
Saad Nasir <mn0871590@gmail.com>
2026-03-26 17:20:56 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const domain = process.env.NEXT_PUBLIC_DOMAIN!;
-  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: `${domain}/sitemap.xml`,
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://qari-spot.vercel.app/sitemap.xml',
   }
 }
