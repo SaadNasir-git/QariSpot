8e867c78c78045ead17a4c6660c065dcc0779feb
Saad Nasir <mn0871590@gmail.com>
2026-03-29 15:40:16 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/_next/static/chunks/app/',
+    },
     sitemap: 'https://qari-spot.vercel.app/sitemap.xml',
   }
 }
